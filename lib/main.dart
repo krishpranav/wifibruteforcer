f454c96b2ad8e83bed557595cfa00ae2e42301ae
krishpranav <krisna.pranav@gmail.com>
2021-07-29 16:22:33 +0530

    lib: main

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,174 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:wifi_iot/wifi_iot.dart';
+import 'dart:io' show Platform;
+import 'package:wifibruteforce/bruteforce.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() => runApp(MaterialApp(darkTheme: ThemeData.dark(), debugShowCheckedModeBanner: false, home: FlutterWifiIoT()));
+
+class FlutterWifiIoT extends StatefulWidget {
+  @override
+  _FlutterWifiIoTState createState() => _FlutterWifiIoTState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
+class _FlutterWifiIoTState extends State<FlutterWifiIoT> {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+  Widget build(BuildContext poContext) {
+    return Scaffold(body: getWidgets(poContext));
+  }
+
+  List<WifiNetwork> _htResultNetwork = [];
+  bool _isEnabled = false;
+  bool _isConnected = false;
+  String ssid = "";
+  @override
+  initState() {
+    getWifis();
+
+    super.initState();
+  }
+
+  getWifis() async {
+    _isEnabled = await WiFiForIoTPlugin.isEnabled();
+    _isConnected = await WiFiForIoTPlugin.isConnected();
+    _htResultNetwork = await loadWifiList();
+    setState(() {});
+    if (_isConnected) {
+      WiFiForIoTPlugin.getSSID().then((value) => setState(() {
+            ssid = value;
+          }));
+    }
+  }
+
+  Future<List<APClient>> getClientList(bool onlyReachables, int reachableTimeout) async {
+    List<APClient> htResultClient;
+    try {
+      htResultClient = await WiFiForIoTPlugin.getClientList(onlyReachables, reachableTimeout);
+    } on PlatformException {
+      htResultClient = List<APClient>();
+    }
+
+    return htResultClient;
+  }
+
+  Future<List<WifiNetwork>> loadWifiList() async {
+    List<WifiNetwork> htResultNetwork;
+    try {
+      htResultNetwork = await WiFiForIoTPlugin.loadWifiList();
+    } on PlatformException {
+      htResultNetwork = List<WifiNetwork>();
+    }
+
+    return htResultNetwork;
+  }
+
+  isRegisteredWifiNetwork(String ssid) {
+    return ssid == this.ssid;
+  }
+
+  Widget getWidgets(context) {
+    WiFiForIoTPlugin.isConnected().then((val) => setState(() {
+          _isConnected = val;
+        }));
+
+    return SingleChildScrollView(
+      child: Column(
+        children: getButtonWidgetsForAndroid(context),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+  List<Widget> getButtonWidgetsForAndroid(context) {
+    List<Widget> htPrimaryWidgets = List();
+    WiFiForIoTPlugin.isEnabled().then((val) => setState(() {
+          _isEnabled = val;
+        }));
+    htPrimaryWidgets.addAll({
+      Container(
+        color: Colors.blue,
+        height: 350,
+        child: Center(
+          child: Text(
+            'Wifi BruteForce Tool',
+            style: TextStyle(color: Colors.black54, fontSize: 35),
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      Container(
+        child: ListTile(
+            leading: Text('Wi-Fi'),
+            trailing: Switch(
+                value: _isEnabled,
+                onChanged: (v) {
+                  if (_isEnabled) {
+                    WiFiForIoTPlugin.setEnabled(false);
+                  } else {
+                    WiFiForIoTPlugin.setEnabled(true);
+                    getWifis();
+                  }
+                  setState(() {
+                    _isEnabled = !_isEnabled;
+                  });
+                })),
+        color: _isEnabled ? Colors.green : Colors.red,
+      ),
+      SizedBox(height: 10),
+      Text(
+        'Wi-Fis Found',
+        style: TextStyle(fontSize: 25),
+        textAlign: TextAlign.center,
+      ),
+      IconButton(
+          icon: Icon(Icons.refresh),
+          onPressed: () {
+            getWifis();
+          }),
+      getList(context)
+    });
+    if (_isEnabled) {
+      WiFiForIoTPlugin.isConnected().then((val) {
+        if (val != null) {
+          setState(() {
+            _isConnected = val;
+          });
+        }
+      });
+    }
+
+    return htPrimaryWidgets;
+  }
+
+  getList(contex) {
+    return ListView.builder(
+      itemBuilder: (builder, i) {
+        var network = _htResultNetwork[i];
+        var isConnctedWifi = false;
+        if (_isConnected) isConnctedWifi = isRegisteredWifiNetwork(network.ssid);
+
+        if (_htResultNetwork != null && _htResultNetwork.length > 0) {
+          return Container(
+            color: isConnctedWifi ? Colors.blueAccent : Colors.transparent,
+            child: ListTile(
+                title: Text(network.ssid),
+                trailing: !isConnctedWifi
+                    ? OutlineButton(
+                        onPressed: () {
+                          Navigator.of(contex).push(MaterialPageRoute(
+                              builder: (_) => Attack(
+                                    wifiNetwork: network,
+                                  )));
+                        },
+                        child: Text('Attack'),
+                      )
+                    : SizedBox()),
+          );
+        } else
+          return Center(
+            child: Text('No wifi found'),
+          );
+      },
+      itemCount: _htResultNetwork.length,
+      shrinkWrap: true,
     );
   }
 }
