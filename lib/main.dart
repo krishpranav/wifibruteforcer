93b287326b8852804649329f7b83f0676e5264ad
krishpranav <krisna.pranav@gmail.com>
2021-07-29 17:25:06 +0530

    code update

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:wifi_iot/wifi_iot.dart';
 import 'dart:io' show Platform;
 import 'package:wifibruteforce/bruteforce/bruteforce.dart';
+import 'package:wifibruteforce/screens/agreement.dart';
 
 void main() => runApp(MaterialApp(darkTheme: ThemeData.dark(), debugShowCheckedModeBanner: false, home: FlutterWifiIoT()));
 
@@ -14,6 +15,31 @@ class FlutterWifiIoT extends StatefulWidget {
 class _FlutterWifiIoTState extends State<FlutterWifiIoT> {
   @override
   Widget build(BuildContext poContext) {
-    return Container(child: Text('Hello World'));
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        // title: Text("Attacking " + widget.wifiNetwork.ssid),
+        title: Text("Text"),
+      ),
+      body: Column(
+        children: <Widget>[
+          Image(image: AssetImage('assets/hack.jpg')),
+          SizedBox(
+            height: 15,
+          ),
+          Center(
+            child: SelectableText(
+              "Wifi Bruteforcer  ",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 35),
+            ),
+          ),
+          SizedBox(
+            height: 190,
+          ),
+          Center(child: Text('Wifi BruteForcer app built by krisna pranav', style: TextStyle(fontSize: 10))),
+        ],
+      ),
+    );
   }
 }
